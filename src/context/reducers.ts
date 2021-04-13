a8d782e133e9a0f93783fb1fb89f335b560e4e84
Luka Kerečeni <luka.kereceni@gmail.com>
2021-04-13 17:38:54 +0200

    Modified context

--- src/context/reducers.ts
+++ src/context/reducers.ts
@@ -1,26 +1,17 @@
 import {
-    FETCH_POSTS_REQUEST, 
-    FETCH_POSTS_SUCCESS,
-    FETCH_POSTS_FAIL,
-    FETCH_USERS_SUCCESS,
-    FETCH_USERS_REQUEST,
-    FETCH_USERS_FAIL,
+    FETCH_DATA_REQUEST,
+    FETCH_DATA_SUCCESS,
+    FETCH_DATA_FAIL,
 } from './constants';
 
-const globalReducer = (state: IState, action: IAction) => {
+const globalReducer = (state: IStore, action: IAction) => {
     switch (action.type) {
-        case FETCH_POSTS_REQUEST:
-            return { ...state, posts: { ...state.posts, loading: true } };
-        case FETCH_POSTS_SUCCESS:
-            return { ...state, posts: { ...state.posts, loading: false, data: action.payload } };
-        case FETCH_POSTS_FAIL:
-            return { ...state, posts: { ...state.posts, loading: false, error: action.payload } };
-        case FETCH_USERS_REQUEST:
-            return { ...state, users: { ...state.users, loading: true } };
-        case FETCH_USERS_SUCCESS:
-            return { ...state, users: { ...state.users, loading: false, data: action.payload } };
-        case FETCH_USERS_FAIL:
-            return { ...state, users: { ...state.users, loading: false, error: action.payload } };
+        case FETCH_DATA_REQUEST:
+            return { ...state, loading: true };
+        case FETCH_DATA_SUCCESS:
+            return { ...state, loading: false, data: { ...action.payload } };
+        case FETCH_DATA_FAIL:
+            return { ...state, loading: false, error: action.payload };
         default:
             return state;
     }
--- src/context/store.tsx
+++ src/context/store.tsx
@@ -1,58 +1,67 @@
 import { useReducer, createContext } from 'react';
 import axios from 'axios';
 
-// reducers
-import reducer from './reducers';
-
 // constants
 import {
-    FETCH_POSTS_REQUEST, 
-    FETCH_POSTS_SUCCESS, 
-    FETCH_POSTS_FAIL,
-    FETCH_USERS_REQUEST,
-    FETCH_USERS_SUCCESS,
+    FETCH_DATA_REQUEST, 
+    FETCH_DATA_SUCCESS, 
+    FETCH_DATA_FAIL,
 } from './constants';
+
+// reducers
+import reducer from './reducers';
 
 // services
 import api from '../services/api';
 
-const initialState: IState = {
-    posts: { loading: null, data: [], error: null },
-    comments: { loading: null, data: [], error: null },
-    users: { loading: null, data: [], error: null },
+const initialState: IStore = {
+    loading: null,
+    error: null,
+    data: {
+        posts: [],
+        comments: [],
+        users: [],
+    },
 }
 
-export const Store = createContext<IState | any>(initialState);
+export const Store = createContext<IStore | any>(initialState);
 
 export const StoreProvider: React.FC = (props: any) => {
 
     const [state, dispatch] = useReducer(reducer, initialState);
 
-    const fetchPosts = async () => {
+    const fetchData = async () => {
 
-        dispatch({ type: FETCH_POSTS_REQUEST });
-        dispatch({ type: FETCH_USERS_REQUEST });
+        dispatch({ type: FETCH_DATA_REQUEST });
 
         try {
 
-            const data = await axios.all([
+            const fetchedData = await axios.all([
                 api.get('/posts'),
+                api.get('/comments'),
                 api.get('/users'),
             ]);
 
-            dispatch({ type: FETCH_POSTS_SUCCESS, payload: data[0].data });
-            dispatch({ type: FETCH_USERS_SUCCESS, payload: data[1].data });
+            const data = {
+                posts: fetchedData[0].data,
+                comments: fetchedData[1].data,
+                users: fetchedData[2].data,
+            }
+
+            dispatch({ type: FETCH_DATA_SUCCESS, payload: data });
 
         } catch (error) {
-            dispatch({ type: FETCH_POSTS_FAIL, payload: error });
+            dispatch({ type: FETCH_DATA_FAIL, payload: 'An error occurred while fetching data.' });
         }
     }
 
     const value = {
-        posts: state.posts,
-        comments: state.comments,
-        users: state.users,
-        fetchPosts,
+        posts: state.data.posts,
+        comments: state.data.comments,
+        users: state.data.users,
+        loading: state.loading,
+        error: state.error,
+        fetchData,
     }
 
     return (
